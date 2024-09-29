5cf37662ca113fd5f5d04df627583266c13fdd7b
jipsa-7 <jipsa.varkey@yahoo.com>
2024-09-28 21:46:27 -0500

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom";
+const lucknum = 7;
+ReactDOM.render(
+  <div>
+    <h1>Hello World!</h1>
+    <p>My luckynuber is {lucknum}</p>
+  </div>,
+  document.getElementById("root")
+);
